414f3b5af64ba67f10c12f5715fb11ab0f2b8d08
ange07001 <ange07001@utb.vaxjo.se>
2025-02-11 21:54:59 +0100

    Improved variable names and structure

--- iot.py
+++ iot.py
@@ -1,52 +1,50 @@
-# imports
+# ------ Imports ------
 import time
 import board
 import busio as bus
 import pwmio as pwm
-import digitalio as io
+import digitalio as dio
+import analogio as aio
 from adafruit_ina219 import ADCResolution, BusVoltageRange, INA219
 
+# ------ Functions ------
 
-# functions
-def initPin(pin, direction, pull=None):
+def initPin(boardPin, direction, pull=None):
 
-    dio = io.DigitalInOut(pin)
+    pin = dio.DigitalInOut(boardPin)
 
     if direction == 'output':
-        dio.direction = io.Direction.OUTPUT
+        pin.direction = dio.Direction.OUTPUT
     elif direction == 'input':
-        dio.direction = io.Direction.INPUT
+        pin.direction = dio.Direction.INPUT
 
 
         if pull == 'up':
-            dio.pull = io.Pull.UP
+            pin.pull = dio.Pull.UP
         elif pull == 'down':
-            dio.pull = io.Pull.DOWN
+            pin.pull = dio.Pull.DOWN
 
-    return dio
+    return pin
 
-def motor(motor, speed, direction, debug = False):
+def motor(speed, direction, debug = False):
 
     pwmSpeed = round((speed/100)*65535)
 
-    if(motor == 1):
+    
         en1.duty_cycle = pwmSpeed
+        en2.duty_cycle = pwmSpeed
         if(direction == "f"):
             in1.value = True
             in2.value = False
-        elif(direction == "r"):
-            in1.value = False
-            in2.value = True
-    elif(motor == 2):
-        en2.duty_cycle = pwmSpeed
-        if(direction == "f"):
             in3.value = True
             in4.value = False
         elif(direction == "r"):
+            in1.value = False
+            in2.value = True
             in3.value = False
             in4.value = True
 
-    if(bool(debug) == True):
+    if debug:
         print(f"""
         en1: {en1.duty_cycle}
         en2: {en2.duty_cycle}
@@ -56,43 +54,23 @@ def motor(motor, speed, direction, debug = False):
         in4: {in4.value}
         """)
         
-def motorStop(motor):
-    if (motor == 1):
+def motorStop(debug = False):
         en1.duty_cycle = 0
+        en2.duty_cycle = 0
         in1.value = False
         in2.value = False
-    elif (motor == 2):
-        en2.duty_cycle = 0
         in3.value = False
         in4.value = False
         
-def measureINA219(sensor):
-    bus_voltage = sensor.bus_voltage  # voltage on V- (load side)
-    shunt_voltage = sensor.shunt_voltage  # voltage between V+ and V- across the shunt
-    current = sensor.current  # current in mA
-    power = sensor.power  # power in watts
-    return (bus_voltage,shunt_voltage,current,power)
+        if debug:
+            print("Motors are stopped")
+            
+# ------ Definitions ------
 
-def servoDeg(deg):
-    min_duty = 1638 
-    max_duty = 8192 
-    return round(min_duty + (deg / 180) * (max_duty - min_duty))
+# Time
+programStartTime = time.monotonic()
 
-def shift(direction):
-    shiftUpDeg = 23
-    shiftDownDeg = 150
-    shiftMiddleDeg = 75
-    if (direction == "up"):
-        servo.duty_cycle = (shiftUpDeg)
-        time.sleep(0.2)
-        servo.duty_cycle = servoDeg(shiftMiddleDeg)
-    elif (direction == "down"):
-        servo.duty_cycle = servoDeg(shiftDownDeg)
-        time.sleep(0.2)
-        servo.duty_cycle = servoDeg(shiftMiddleDeg)
-
-        
-# define pins and initialize
+# Motor
 en1 = pwm.PWMOut(board.GP21, frequency=5000, duty_cycle=0)
 en2 = pwm.PWMOut(board.GP20, frequency=5000, duty_cycle=0)
 
@@ -101,51 +79,44 @@ in2 = initPin(board.GP18, "output")
 in3 = initPin(board.GP17, "output")
 in4 = initPin(board.GP16, "output")
 
-vibrationSensor = initPin(board.GP15, "input")
-
-i2c_bus = bus.I2C(board.GP5,board.GP4)
-
-ina219 = INA219(i2c_bus)
-
+# Servo
 servo = pwm.PWMOut(board.GP14, frequency=50, duty_cycle=servoDeg(75))
 
+# Power sensor
+i2c_bus = bus.I2C(board.GP5,board.GP4)
+ina219 = INA219(i2c_bus)
 
-# setup INA219
 ina219.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
 ina219.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
 ina219.bus_voltage_range = BusVoltageRange.RANGE_16V
 
-# program loop
-while True:
-    """sensorReading = measureINA219(ina219)
-    
-    if vibrationSensor.value:
-        print("Vibration Detected")
-    
-    print("Voltage (VIN+) : {:6.3f}   V".format(sensorReading[0] + sensorReading[1]))
-    print("Voltage (VIN-) : {:6.3f}   V".format(sensorReading[0]))
-    print("Shunt Voltage  : {:8.5f} V".format(sensorReading[1]))
-    print("Shunt Current  : {:7.4f}  A".format(sensorReading[2] / 1000))
-    print("Power Register : {:6.3f}   W".format(sensorReading[3]))
-    print("")
+# Vibration sensor
+vibrationSensor = initPin(board.GP15, "input")
 
-    
-    if ina219.overflow:
-        print("Internal Math Overflow Detected!")
-        print("")
-    """
-    motor(1,100,"f",True)
-    motor(2,100,"f",True)
-    
-    time.sleep(10)
-    motorStop(1)
-    motorStop(2)
-    break
-    
-    """
-    if (float(sensorReading[0]) <= 9.6):
-        print("VOLTAGE LOW! SHUTTING DOWN")
-        motor(1,0,"f",True)
-        motor(2,0,"f",True)
-        break"""
-    
+# Hall effect sensor connected to motor
+hallMotor = aio.AnalogIn(board.GP28)
+
+# Hall effect sensor connected to output
+hallOut = aio.AnalogIn(board.GP27)
+
+# ------ Variables ------
+
+# Hall effect sensor to motor
+hallMotorRpm = 0
+hallMotorTriggered = False
+hallMotorMoved = False
+hallMotorStartTime = None
+MotorLastRpmUpdate = programStartTime
+
+# Hall effect sensor to output
+hallOutRpm = 0
+hallOutTriggered = False
+hallOutMoved = False
+hallOutStartTime = None  # Init
+OutlastRpmUpdate = programStartTime  # Track last RPM update
+
+
+
+# ------ Loop ------
+while True:
+        currentTime = time.monotonic()
